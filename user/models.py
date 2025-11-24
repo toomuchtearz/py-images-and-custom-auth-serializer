4144676f287d072c3b7f4e9f3df03369fbdf1639
toomuchtearz <toomuchtearz@gmail.com>
2025-11-24 18:17:12 +0200

    update models.py

--- user/models.py
+++ user/models.py
@@ -26,18 +26,17 @@ class UserManager(DjangoUserManager):
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-def create_superuser(self, email, password, **extra_fields):
-    extra_fields.setdefault("is_staff", True)
-    extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
 
-    if extra_fields.get("is_staff") is not True:
-        raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
-    if extra_fields.get("is_superuser") is not True:
-        raise ValueError("Superuser must have is_superuser=True.")
-
-    return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
